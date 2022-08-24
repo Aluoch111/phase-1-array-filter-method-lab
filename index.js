e3f0b58f574bb67e12700890613629cf41abd589
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-08-24 09:26:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(drivers,n){
+   let driver = drivers.filter((match) => match.toLowerCase() === n.toLowerCase());
+   return  driver ;
+}
+function fuzzyMatch(drivers,n){
+   let driver = drivers.filter((match) => match.toLowerCase().indexOf(n.toLowerCase()) === 0);
+    return driver;
+}
+
+function matchName(drivers, n) {
+   return drivers.filter((match) => match.name === n);
+ }
